1417356979aa697a3494d416179f0a251a5f4417
Luke Leonard (Carly) <lukeleonard.coder@gmail.com>
2019-10-30 08:40:38 -0600

    finish jthw ex 1

---
+++ JavaTheHardWay/FirstProg.java
@@ -0,0 +1,8 @@
+public class FirstProg
+{
+  public static void main( String[] args)
+  {
+    System.out.println("I, Luke Leonard, am determined to learn how to code.");
+    System.out.println("Today's date is October 30, 2019");
+  }
+}
