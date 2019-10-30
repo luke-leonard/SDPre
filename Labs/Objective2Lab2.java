c389e7e9d8898fde8164ff3f75caa8a5e5c3adaf
Luke Leonard (Carly) <lukeleonard.coder@gmail.com>
2019-10-30 12:50:04 -0600

    Objective 2 Lab 1-5

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,12 @@
+public class Objective2Lab2
+{
+  public static void main(String[] args)
+  {
+    String name = "Luke Leonard";
+    int age = 21;
+    String state = "California";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state );
+
+  }
+}
---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,13 @@
+public class Objective2Lab3
+{
+  public static void main(String[] args)
+  {
+    String food1, food2, food3;
+    food1 = "Pizza";
+    food2 = "Burgers";
+    food3 = "Chicken";
+
+    System.out.println("My Favorite Foods Are:");
+    System.out.println("1. " + food1 + "\n2. " + food2 + "\n3. " + food3);
+  }
+}
