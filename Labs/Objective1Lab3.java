befb5fb13281145f498d6acfe0f3407013db51b8
Luke Leonard (Carly) <lukeleonard.coder@gmail.com>
2019-10-30 10:09:19 -0600

    Objective 1 labs 1-3

---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,9 @@
+public class Objective1Lab3
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Name : Luke Leonard");
+    System.out.println("Age : 21");
+    System.out.println("State : California");
+  }
+}
