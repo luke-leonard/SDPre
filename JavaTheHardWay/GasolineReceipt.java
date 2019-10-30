b66566de258aaa7d8e10aa801f01e0529d41a958
Luke Leonard (Carly) <lukeleonard.coder@gmail.com>
2019-10-30 09:57:47 -0600

    JtHW 2-4

---
+++ JavaTheHardWay/GasolineReceipt.java
@@ -0,0 +1,18 @@
+public class GasolineReceipt
+{
+    public static void main( String[] args )
+    {
+      System.out.println( "+------------------------+" );
+      System.out.println( "|                        |" );
+      System.out.println( "|      CORNER STORE      |" );
+      System.out.println( "|                        |" );
+      System.out.println( "| 2019-08-30  09:01am    |" );
+      System.out.println( "|                        |" );
+      System.out.println( "| Gallons:       10.870  |" );
+      System.out.println( "| Price/gallon: $ 2.789  |" );
+      System.out.println( "|                        |" );
+      System.out.println( "| Fuel total:  $ 30.32   |" );
+      System.out.println( "|                        |" );
+      System.out.println( "+------------------------+" );
+    }
+}
